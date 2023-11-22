430577f7a870a0419d24bef9556eb99da9e0fd50
nbVenti <nbVenti@gmail.com>
2023-11-22 10:26:15 -0800

    Yay

--- eval2/script.js
+++ eval2/script.js
@@ -126,6 +126,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("result").textContent = "";
     
     });
+
     let rock = document.getElementById("rock");
     let paper = document.getElementById("paper");
     let scissors = document.getElementById("scissors");
